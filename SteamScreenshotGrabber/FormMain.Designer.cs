a028a4e11a36350c0145487af176948b2495ac5c
f26 <f26@users.noreply.github.com>
2014-08-15 14:05:00 -0400

    Initial alpha
    Working but with minimal error checking, only single threaded, and all
    of it runs in the GUI thread.  More of a proof of concept, really.

---
+++ SteamScreenshotGrabber/FormMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace SteamScreenshotGrabber
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSteamID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelURL = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPageStop = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBoxPageStart = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxSteamID
+            // 
+            this.textBoxSteamID.Location = new System.Drawing.Point(110, 9);
+            this.textBoxSteamID.Name = "textBoxSteamID";
+            this.textBoxSteamID.Size = new System.Drawing.Size(129, 20);
+            this.textBoxSteamID.TabIndex = 0;
+            this.textBoxSteamID.Text = "76561198003853507";
+            this.textBoxSteamID.TextChanged += new System.EventHandler(this.optionsChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Steam ID:";
+            // 
+            // labelURL
+            // 
+            this.labelURL.AutoSize = true;
+            this.labelURL.Location = new System.Drawing.Point(12, 136);
+            this.labelURL.Name = "labelURL";
+            this.labelURL.Size = new System.Drawing.Size(476, 13);
+            this.labelURL.TabIndex = 2;
+            this.labelURL.Text = "http://steamcommunity.com/profiles/XXXXXXXXXXXXX/screenshots/?p=1&sort=oldestfirs" +
+                "t&view=grid";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(80, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "Newest first";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.optionsChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Oldest first";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.optionsChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 69);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Order";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Pages to process:";
+            // 
+            // textBoxPageStop
+            // 
+            this.textBoxPageStop.Location = new System.Drawing.Point(165, 35);
+            this.textBoxPageStop.Name = "textBoxPageStop";
+            this.textBoxPageStop.Size = new System.Drawing.Size(30, 20);
+            this.textBoxPageStop.TabIndex = 6;
+            this.textBoxPageStop.Text = "65";
+            this.textBoxPageStop.TextChanged += new System.EventHandler(this.optionsChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(438, 7);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 10;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 159);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(519, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel.Text = "Ready";
+            // 
+            // textBoxPageStart
+            // 
+            this.textBoxPageStart.Location = new System.Drawing.Point(110, 35);
+            this.textBoxPageStart.Name = "textBoxPageStart";
+            this.textBoxPageStart.Size = new System.Drawing.Size(30, 20);
+            this.textBoxPageStart.TabIndex = 12;
+            this.textBoxPageStart.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "to";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 181);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPageStart);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPageStop);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSteamID);
+            this.Name = "FormMain";
+            this.Text = "Steam Screenshot Grabber";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSteamID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelURL;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPageStop;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.TextBox textBoxPageStart;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SteamScreenshotGrabber/FormMain.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SteamScreenshotGrabber
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            GenerateURL(Convert.ToInt32(this.textBoxPageStart.Text));
+        }
+
+        private void optionsChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                GenerateURL(Convert.ToInt32(this.textBoxPageStart.Text));
+            }
+            catch
+            {
+                GenerateURL(1);
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //this.toolStripStatusLabel.Text = "Fetching URL";
+                //Application.DoEvents();
+                //this.textBox1.Text = Wget(this.labelURL.Text);
+                //this.toolStripStatusLabel.Text = "Ready";
+
+                int startPage = Convert.ToInt32(this.textBoxPageStart.Text);
+                int endPage = Convert.ToInt32(this.textBoxPageStop.Text);
+
+                // 
+                for (int i = startPage; i <= endPage; i++)
+                {
+                    UpdateStatus("Generating URL");
+                    GenerateURL(i);
+
+                    UpdateStatus("Fetching page " + i);
+                    Wget(this.labelURL.Text, "albumpage.html");
+                    string[] html = File.ReadAllLines("albumpage.html");
+
+                    UpdateStatus("Parsing image URLs from page " + i);
+                    List<string> imageUrls = ParseImageUrls(html);
+
+                    for (int j = 0; j < imageUrls.Count; j++)
+                    {
+                        UpdateStatus("Fetching image " + (j + 1) + "/" + imageUrls.Count + " from page " + i);
+                        DownloadImage(imageUrls[j]);
+                    }
+                }
+
+                // foreach page:
+
+                // wget url
+
+                // Grab all screenshots in the album page
+                // ParseImageUrls(html)
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void DownloadImage(string imagePageURL)
+        {
+            Wget(imagePageURL, "imagepage.html");
+            string[] lines = File.ReadAllLines("imagepage.html");
+
+            string imageUrl = "";
+            string imageDestinationFilename = "";
+
+            foreach (string line in lines)
+            {
+                // Find the direct URL to the image itself
+                if (line.Contains("ActualMedia"))
+                {
+                    // Grab the direct URL of the image
+                    // Sample line of what I'm expecting:
+                    // \t\t\t\t<a href=\"http://cloud-4.steampowered.com/ugc/595804413485986490/B75F91916AB097CA12337C01BB3B2A7A678A7F45/\" target=\"_blank\"><img id=\"ActualMedia\" class=\"screenshotEnlargeable\" src=\"http://cloud-4.steampowered.com/ugc/595804413485986490/B75F91916AB097CA12337C01BB3B2A7A678A7F45/1024x640.resizedimage\" width=\"1024\"></a>
+                    imageUrl = Regex.Match(line, "http[^\"]+").Captures[0].Value;
+                }
+
+                // Find the timestamp of the image
+                if (line.Contains("detailsStatRight") && line.Contains(" @ "))
+                {
+                    // Sample line of what I'm expecting:
+                    // \t\t\t\t\t\t\t\t<div class=\"detailsStatRight\">Feb 26, 2011 @ 2:40pm</div>
+
+                    // Get the date of the screenshot
+                    string date = Regex.Match(line, "[>]([^<]+)").Captures[0].Value; // ">Feb 26, 2011 @ 2:40pm"
+                    date = date.TrimStart('>'); // "Feb 26, 2011 @ 2:40pm"
+                    date = date.Replace(" @ ", " "); // "Feb 26, 2011 2:40pm"
+                    DateTime dt = DateTime.Parse(date);
+
+                    // Generate a filename (this is needed because there can be multiple screenshots taken the same minute)
+                    int number = 0;
+                    do
+                    {
+                        imageDestinationFilename = dt.ToString("yyyy-MM-dd HH-mm-") + number.ToString("D2") + ".jpg";
+                        number++;
+                    }
+                    while (File.Exists(imageDestinationFilename));
+                }
+            }
+
+            // Download the screenshot
+            Wget(imageUrl, imageDestinationFilename);
+        }
+
+        private List<string> ParseImageUrls(string[] html)
+        {
+             List<string> urls = new List<string>();
+
+            foreach (string line in html)
+            {
+                if (!line.Contains("http://steamcommunity.com/sharedfiles/filedetails/?id="))
+                    continue;
+
+                string pattern = "http://steamcommunity.com/sharedfiles/filedetails/[?]id=[0-9]+";
+                Match m = Regex.Match(line, pattern);
+                string result = m.Captures[0].Value;
+                //Wget(result);
+                urls.Add(result);
+            }
+
+            return urls;
+        }
+
+        private void GenerateURL(int pageNum)
+        {
+            string order = "newest";
+            if (this.radioButton2.Checked) order = "oldest";
+
+            // Create a URL
+            // Sample: http://steamcommunity.com/profiles/76561198003853507/screenshots/?p=1&sort=oldestfirst&view=grid
+            this.labelURL.Text = "http://steamcommunity.com/profiles/" +
+                this.textBoxSteamID.Text + "/screenshots/?p=" +
+                pageNum +
+                "&sort=" +
+                order +
+                "first&view=grid";
+        }
+
+        // Grabbed from:  http://stackoverflow.com/questions/26233/fastest-c-sharp-code-to-download-a-web-page
+        public string Wget(string url)
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile(url, "imagepage.html");
+            return client.DownloadString(url);
+        }
+        public void Wget(string url, string localFilename)
+        {
+            WebClient client = new WebClient();
+            if (File.Exists(localFilename)) File.Delete(localFilename);
+            client.DownloadFile(url, localFilename);
+        }
+
+        private void UpdateStatus(string msg)
+        {
+            this.toolStripStatusLabel.Text = msg;
+            Application.DoEvents();
+        }
+
+    }
+}
