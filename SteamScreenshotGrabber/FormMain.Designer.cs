37211a2386aedebc2cc17f431aae6bf17fc1805e
f26 <f26@users.noreply.github.com>
2014-08-15 15:20:06 -0400

    Initial beta
    Download thread now runs in background, preventing GUI from locking up.
    Ability to stop added.  More status added.

--- SteamScreenshotGrabber/FormMain.Designer.cs
+++ SteamScreenshotGrabber/FormMain.Designer.cs
@@ -30,19 +30,16 @@
         {
             this.textBoxSteamID = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.labelURL = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBoxPageStop = new System.Windows.Forms.TextBox();
             this.buttonStart = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.textBoxPageStart = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
-            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxSteamID
@@ -52,7 +49,6 @@
             this.textBoxSteamID.Size = new System.Drawing.Size(129, 20);
             this.textBoxSteamID.TabIndex = 0;
             this.textBoxSteamID.Text = "76561198003853507";
-            this.textBoxSteamID.TextChanged += new System.EventHandler(this.optionsChanged);
             // 
             // label1
             // 
@@ -63,16 +59,6 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Steam ID:";
             // 
-            // labelURL
-            // 
-            this.labelURL.AutoSize = true;
-            this.labelURL.Location = new System.Drawing.Point(12, 136);
-            this.labelURL.Name = "labelURL";
-            this.labelURL.Size = new System.Drawing.Size(476, 13);
-            this.labelURL.TabIndex = 2;
-            this.labelURL.Text = "http://steamcommunity.com/profiles/XXXXXXXXXXXXX/screenshots/?p=1&sort=oldestfirs" +
-                "t&view=grid";
-            // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
@@ -82,7 +68,6 @@
             this.radioButton1.TabIndex = 3;
             this.radioButton1.Text = "Newest first";
             this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.optionsChanged);
             // 
             // radioButton2
             // 
@@ -95,7 +80,6 @@
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Oldest first";
             this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.optionsChanged);
             // 
             // groupBox1
             // 
@@ -124,7 +108,6 @@
             this.textBoxPageStop.Size = new System.Drawing.Size(30, 20);
             this.textBoxPageStop.TabIndex = 6;
             this.textBoxPageStop.Text = "65";
-            this.textBoxPageStop.TextChanged += new System.EventHandler(this.optionsChanged);
             // 
             // buttonStart
             // 
@@ -135,22 +118,6 @@
             this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 159);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(519, 22);
-            this.statusStrip1.TabIndex = 11;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripStatusLabel
-            // 
-            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
-            this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
-            this.toolStripStatusLabel.Text = "Ready";
             // 
             // textBoxPageStart
             // 
@@ -169,27 +136,36 @@
             this.label2.TabIndex = 13;
             this.label2.Text = "to";
             // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 135);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(495, 198);
+            this.richTextBox.TabIndex = 14;
+            this.richTextBox.Text = "";
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 181);
+            this.ClientSize = new System.Drawing.Size(532, 362);
+            this.Controls.Add(this.richTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBoxPageStart);
-            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBoxPageStop);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.labelURL);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxSteamID);
+            this.MinimumSize = new System.Drawing.Size(530, 400);
             this.Name = "FormMain";
             this.Text = "Steam Screenshot Grabber";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,17 +175,15 @@
 
         private System.Windows.Forms.TextBox textBoxSteamID;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label labelURL;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBoxPageStop;
         private System.Windows.Forms.Button buttonStart;
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
         private System.Windows.Forms.TextBox textBoxPageStart;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox;
     }
 }
 
--- SteamScreenshotGrabber/FormMain.cs
+++ SteamScreenshotGrabber/FormMain.cs
@@ -14,70 +14,139 @@ namespace SteamScreenshotGrabber
 {
     public partial class FormMain : Form
     {
+
+        BackgroundWorker bgWorker = null; // The background thread that does the downloading
+        int startPage = 0; // The first page to download
+        int endPage = 0; // The last page to download
+        string albumPageUrl = ""; // THe URL of the current album page
+
+        /// <summary>
+        /// Boring constructor is boring...
+        /// </summary>
         public FormMain()
         {
             InitializeComponent();
-            GenerateURL(Convert.ToInt32(this.textBoxPageStart.Text));
         }
 
-        private void optionsChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                GenerateURL(Convert.ToInt32(this.textBoxPageStart.Text));
-            }
-            catch
-            {
-                GenerateURL(1);
-            }
-        }
-
+        /// <summary>
+        /// Either kicks off the download thread or stops it, depending on what's currently going on.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonStart_Click(object sender, EventArgs e)
         {
             try
             {
-                //this.toolStripStatusLabel.Text = "Fetching URL";
-                //Application.DoEvents();
-                //this.textBox1.Text = Wget(this.labelURL.Text);
-                //this.toolStripStatusLabel.Text = "Ready";
-
-                int startPage = Convert.ToInt32(this.textBoxPageStart.Text);
-                int endPage = Convert.ToInt32(this.textBoxPageStop.Text);
-
-                // 
-                for (int i = startPage; i <= endPage; i++)
+                if (this.bgWorker == null)
                 {
-                    UpdateStatus("Generating URL");
-                    GenerateURL(i);
+                    this.buttonStart.Text = "Stop";
 
-                    UpdateStatus("Fetching page " + i);
-                    Wget(this.labelURL.Text, "albumpage.html");
-                    string[] html = File.ReadAllLines("albumpage.html");
+                    this.startPage = Convert.ToInt32(this.textBoxPageStart.Text);
+                    this.endPage = Convert.ToInt32(this.textBoxPageStart.Text);
 
-                    UpdateStatus("Parsing image URLs from page " + i);
-                    List<string> imageUrls = ParseImageUrls(html);
-
-                    for (int j = 0; j < imageUrls.Count; j++)
-                    {
-                        UpdateStatus("Fetching image " + (j + 1) + "/" + imageUrls.Count + " from page " + i);
-                        DownloadImage(imageUrls[j]);
-                    }
+                    // Start up the worker thread
+                    this.bgWorker = new BackgroundWorker();
+                    this.bgWorker.WorkerReportsProgress = true;
+                    this.bgWorker.WorkerSupportsCancellation = true;
+                    this.bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_RunWorkerCompleted);
+                    this.bgWorker.ProgressChanged += new ProgressChangedEventHandler(bgWorker_ProgressChanged);
+                    this.bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+                    this.bgWorker.RunWorkerAsync();
                 }
-
-                // foreach page:
-
-                // wget url
-
-                // Grab all screenshots in the album page
-                // ParseImageUrls(html)
-
+                else
+                {
+                    // Stop the worker thread
+                    this.bgWorker.CancelAsync();
+                }
             }
             catch (Exception ex)
             {
+                MessageBox.Show(ex.ToString());
             }
         }
 
-        private void DownloadImage(string imagePageURL)
+        /// <summary>
+        /// The guts of the download thread.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int startPage = this.startPage;
+            int endPage = this.endPage;
+
+            for (int i = startPage; i <= endPage; i++)
+            {
+                if (this.bgWorker.CancellationPending)
+                {
+                    this.bgWorker.ReportProgress(0, "Cancelling...");
+                    return;
+                }
+
+                // Generate the URL to the current album page
+                this.bgWorker.ReportProgress(0, "Generating album page URL...");
+                GenerateURL(i);
+                this.bgWorker.ReportProgress(0, "Generated: " + albumPageUrl);
+
+                // Download the current album page
+                this.bgWorker.ReportProgress(0, "Downloading album page " + i + "...");
+                Wget(this.albumPageUrl, "albumpage.html");
+                string[] html = File.ReadAllLines("albumpage.html");
+
+                // Parse all screenshot page URLs from the album page
+                this.bgWorker.ReportProgress(0, "Seraching for screenshot URLs on page " + i + "...");
+                List<string> imageUrls = ParseImageUrls(html);
+                this.bgWorker.ReportProgress(0, "Found " + imageUrls.Count + " screenshots on this page.");
+
+                // Download each image
+                this.bgWorker.ReportProgress(0, "Downloading screenshots...");
+                for (int j = 0; j < imageUrls.Count; j++)
+                {
+                    this.bgWorker.ReportProgress(0, "Downloading screenshot " + (j + 1) + "/" + imageUrls.Count + " from page " + i + "...");
+                    string imgFile = DownloadImage(imageUrls[j]);
+                    this.bgWorker.ReportProgress(0, "Screenshot saved to: " + imgFile);
+
+                    if (this.bgWorker.CancellationPending)
+                    {
+                        this.bgWorker.ReportProgress(0, "Cancelling...");
+                        return;
+                    }
+                }
+            }
+
+            this.bgWorker.ReportProgress(0, "Finished downloading all screenshots.");
+        }
+
+        /// <summary>
+        /// Used by the download thread to update the GUI
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.richTextBox.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + (string)e.UserState + "\r\n");
+            this.richTextBox.ScrollToCaret();
+        }
+
+        /// <summary>
+        /// Runs after the download thread is done, resets the GUI
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.richTextBox.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "Done\r\n");
+            this.richTextBox.ScrollToCaret();
+            this.bgWorker = null;
+            this.buttonStart.Text = "Start";
+        }
+
+        /// <summary>
+        /// Given a URL to the page that contains a screenshot, downloads the image and names it appropriately
+        /// </summary>
+        /// <param name="imagePageURL"></param>
+        /// <returns></returns>
+        private string DownloadImage(string imagePageURL)
         {
             Wget(imagePageURL, "imagepage.html");
             string[] lines = File.ReadAllLines("imagepage.html");
@@ -121,11 +190,17 @@ namespace SteamScreenshotGrabber
 
             // Download the screenshot
             Wget(imageUrl, imageDestinationFilename);
+            return imageDestinationFilename;
         }
 
+        /// <summary>
+        /// Finds all image URLs in the provided HTML
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
         private List<string> ParseImageUrls(string[] html)
         {
-             List<string> urls = new List<string>();
+            List<string> urls = new List<string>();
 
             foreach (string line in html)
             {
@@ -142,14 +217,21 @@ namespace SteamScreenshotGrabber
             return urls;
         }
 
+        /// <summary>
+        /// Generates a URL to the appropriate screenshot album page
+        /// </summary>
+        /// <param name="pageNum"></param>
         private void GenerateURL(int pageNum)
         {
             string order = "newest";
             if (this.radioButton2.Checked) order = "oldest";
 
+            this.startPage = Convert.ToInt32(this.textBoxPageStart.Text);
+            this.endPage = Convert.ToInt32(this.textBoxPageStop.Text);
+
             // Create a URL
             // Sample: http://steamcommunity.com/profiles/76561198003853507/screenshots/?p=1&sort=oldestfirst&view=grid
-            this.labelURL.Text = "http://steamcommunity.com/profiles/" +
+            this.albumPageUrl = "http://steamcommunity.com/profiles/" +
                 this.textBoxSteamID.Text + "/screenshots/?p=" +
                 pageNum +
                 "&sort=" +
@@ -157,7 +239,7 @@ namespace SteamScreenshotGrabber
                 "first&view=grid";
         }
 
-        // Grabbed from:  http://stackoverflow.com/questions/26233/fastest-c-sharp-code-to-download-a-web-page
+        // Thanks to:  http://stackoverflow.com/questions/26233/fastest-c-sharp-code-to-download-a-web-page
         public string Wget(string url)
         {
             WebClient client = new WebClient();
@@ -171,11 +253,44 @@ namespace SteamScreenshotGrabber
             client.DownloadFile(url, localFilename);
         }
 
-        private void UpdateStatus(string msg)
+        /// <summary>
+        /// Keeps the GUI sized as the size changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Resize(object sender, EventArgs e)
         {
-            this.toolStripStatusLabel.Text = msg;
-            Application.DoEvents();
+            this.richTextBox.Width = this.ClientSize.Width - this.richTextBox.Left * 2;
+            this.richTextBox.Height = this.ClientSize.Height - this.richTextBox.Top - this.richTextBox.Left;
+            this.buttonStart.Left = this.ClientSize.Width - this.buttonStart.Width - this.richTextBox.Left;
         }
 
+        /// <summary>
+        /// Keeps the GUI sized as the size changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            FormMain_Resize(null, null);
+        }
+
+        /// <summary>
+        /// Attempt to shut down the background thread on shutdown
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                this.bgWorker.CancelAsync();
+            }
+            catch
+            {
+                // Either it wasn't running or we couldn't cancel it
+                // Oh well, at least we tried...
+            }
+        }
     }
 }
